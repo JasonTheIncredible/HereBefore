bab79b17d2994ca82de5eb5605d605b49ccf38d8
JasonTheIncredible <35417691+JasonTheIncredible@users.noreply.github.com>
2019-07-21 03:57:01 -0700

    Created a page to sign up new users, and added error messages for the sign in / sign up pages.

--- app/src/main/java/co/clixel/herebefore/signIn.java
+++ app/src/main/java/co/clixel/herebefore/signIn.java
@@ -2,20 +2,26 @@ package co.clixel.herebefore;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class signIn extends AppCompatActivity{
 
+    //TODO: Sign in with Google account.
+
     private FirebaseAuth mAuth;
     private EditText mEmail, mPassword;
-    private Button btnSignIn, btnLogOut, btnGoToCreateAccount;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,35 +29,47 @@ public class signIn extends AppCompatActivity{
         //declare buttons and edit texts in onCreate
         mEmail = findViewById(R.id.emailAddress);
         mPassword = findViewById(R.id.password);
-        btnSignIn = findViewById(R.id.signInButton);
-        btnGoToCreateAccount = findViewById(R.id.goToCreateAccountButton);
-        btnLogOut = findViewById(R.id.logOutButton);
+        Button btnSignIn = findViewById(R.id.signInButton);
+        Button btnGoToCreateAccount = findViewById(R.id.goToCreateAccountButton);
+        Button btnLogOut = findViewById(R.id.logOutButton);
         mAuth = FirebaseAuth.getInstance();
 
-        //TODO: check if account exists. If not, make a toast message.
-        //TODO: make button for creating account.
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
                 String email = mEmail.getText().toString().toLowerCase();
                 String pass = mPassword.getText().toString();
-                if(!email.equals("") && !pass.equals("")) {
-                    //TODO: add onCompleteListener
-                    mAuth.signInWithEmailAndPassword(email,pass);
-                    toastMessage("Signing in...(unless this account doesn't exist)");
-                    //TODO: move to chat once signed in.
-                    //TODO: check if email is valid
-                }if (email.equals("") && !pass.equals("")) {
-                    toastMessage("Email required");
+                if (email.equals("") && !pass.equals("")) {
+                    toastMessage("Email address required");
                     mEmail.requestFocus();
-                }if (pass.equals("") && !email.equals("")){
+                    return;
+                }if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    toastMessage("Please enter a valid email address");
+                    mEmail.requestFocus();
+                    return;
+                }if (pass.equals("") && !email.equals("")) {
                     toastMessage("Password required");
                     mPassword.requestFocus();
-                }if (pass.equals("") && email.equals("")) {
-                    toastMessage("Please enter your email and password");
-                }else{
-                    toastMessage("Something went wrong");
+                    return;
+                }if (pass.length()<6){
+                    toastMessage("Password must be at least 6 characters long");
+                    mPassword.requestFocus();
+                    return;
                 }
+
+                mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            toastMessage("Signed in");
+                            //TODO: move to chat once signed in.
+                        }if(task.getException() != null){
+                            Toast.makeText(getApplicationContext(), "User Authentication Failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }else{
+                            toastMessage("An unknown error occurred. Please try again.");
+                        }
+                    }
+                });
             }
         });
 
@@ -73,7 +91,6 @@ public class signIn extends AppCompatActivity{
         });
     }
 
-    //TODO: needs to go to chat if user is signed in. If not signed in, needs to go to sign in page.
     protected void updateUI(FirebaseUser currentUser) {
         if (currentUser != null) {
             findViewById(R.id.signInButton).setVisibility(View.GONE);
@@ -92,10 +109,6 @@ public class signIn extends AppCompatActivity{
         updateUI(currentUser);
     }
 
-    /**
-     * * customizable toast
-     * @param message
-     */
     private void toastMessage(String message){
         Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
     }
--- app/src/main/java/co/clixel/herebefore/signUp.java
+++ app/src/main/java/co/clixel/herebefore/signUp.java
@@ -1,6 +1,7 @@
 package co.clixel.herebefore;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Patterns;
 import android.view.View;
@@ -8,14 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class signUp extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private EditText mEmail, mPassword;
-    private Button btnCreateAccount;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +25,7 @@ public class signUp extends AppCompatActivity {
         //declare buttons and edit texts in onCreate
         mEmail = findViewById(R.id.createEmailAddress);
         mPassword = findViewById(R.id.createPassword);
-        btnCreateAccount = findViewById(R.id.createAccountButton);
+        Button btnCreateAccount = findViewById(R.id.createAccountButton);
         mAuth = FirebaseAuth.getInstance();
 
         btnCreateAccount.setOnClickListener(new View.OnClickListener() {
@@ -49,18 +51,23 @@ public class signUp extends AppCompatActivity {
                     return;
                 }
 
-                //TODO: Add onCompleteListener
-                mAuth.createUserWithEmailAndPassword(email,pass);
-                toastMessage("Account created");
+                mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            toastMessage("Signed up");
+                            //TODO: move to chat once signed up.
+                        }if(task.getException() != null){
+                            Toast.makeText(getApplicationContext(), "User Authentication Failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }else{
+                            toastMessage("An unknown error occurred. Please try again.");
+                        }
+                    }
+                });
             }
         });
     }
 
-    /**
-     * * customizable toast
-     *
-     * @param message
-     */
     private void toastMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
