f0cfe2da8607ad4399be27b8d6749e16d5c8119a
JasonTheIncredible <35417691+JasonTheIncredible@users.noreply.github.com>
2019-07-27 22:44:27 -0700

    Changed addValueEventListener to addListenerForSingleEvent and made recyclerView scroll to bottom when chat opens and when keyboard appears.

--- app/src/main/java/co/clixel/herebefore/ChatTest.java
+++ app/src/main/java/co/clixel/herebefore/ChatTest.java
@@ -2,15 +2,17 @@ package co.clixel.herebefore;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.FloatingActionButton;
+import android.view.WindowManager;
 import android.widget.EditText;
-import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,7 +38,7 @@ public class ChatTest extends AppCompatActivity {
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference databaseReference = database.getReference();
-        databaseReference.child("messageThreads").addValueEventListener(new ValueEventListener(){
+        databaseReference.child("messageThreads").addListenerForSingleValueEvent(new ValueEventListener(){
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 // get all of the children at this level
@@ -49,7 +51,37 @@ public class ChatTest extends AppCompatActivity {
                     String messageText = (String) ds.child("message").getValue();
                     mText.add(messageText);
                 }
-                initRecyclerView();
+
+                databaseReference.child("messageThreads").addChildEventListener(new ChildEventListener(){
+                    @Override
+                    public void onChildAdded(DataSnapshot ds, String previousChildName) {
+                        String messageText = (String) ds.child("message").getValue();
+                        mText.add(messageText);
+                        initRecyclerView();
+                        // TODO: update the view with the newest message rather than initialize the whole recyclerView again?
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+
+                });
             }
 
             public void onCancelled(@NonNull DatabaseError databaseError) {
@@ -74,17 +106,33 @@ public class ChatTest extends AppCompatActivity {
 
     private void initRecyclerView(){
         Log.d(TAG, "initRecyclerView: init recyclerView.");
-        RecyclerView recyclerView = findViewById(R.id.messageList);
+        final RecyclerView recyclerView = findViewById(R.id.messageList);
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mUser, mTime, mText);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.scrollToPosition(mText.size() - 1);
+        //TODO: move this line inside addListenerForSingleEvent?
+        recyclerView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                if (bottom < oldBottom) {
+                    recyclerView.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            recyclerView.smoothScrollToPosition(
+                                    recyclerView.getAdapter().getItemCount() - 1);
+                        }
+                    }, 10);
+                }
+            }
+        });
+
     }
 
     @Override
     public void onStart(){
 
         super.onStart();
-
 
     }
 
